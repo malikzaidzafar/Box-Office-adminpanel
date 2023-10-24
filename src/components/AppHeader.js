6aa32c1d54984e3b7a636c0c48343f82ada23113
Anees Ur Rehman <anees.techtimize@gmail.com>
2023-10-24 11:56:34 +0500

    improved code

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,27 +1,10 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import {
-  CContainer,
-  CHeader,
-  CHeaderBrand,
-  CHeaderDivider,
-  CHeaderNav,
-  CHeaderToggler,
-  CNavLink,
-  CNavItem,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilBell, cilEnvelopeOpen, cilList, cilMenu } from '@coreui/icons'
+import { CContainer, CHeader, CHeaderDivider, CHeaderNav } from '@coreui/react'
 
 import { AppBreadcrumb } from './index'
 import { AppHeaderDropdown } from './header/index'
-import { logo } from 'src/assets/brand/logo'
 
 const AppHeader = () => {
-  const dispatch = useDispatch()
-  const sidebarShow = useSelector((state) => state.sidebarShow)
-
   return (
     <CHeader position="sticky" className="mb-4">
       <CContainer fluid>
